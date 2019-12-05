65aabd1bfd8264ea23d41e1b78729a41a50d8512
Zhu Zhen <g.hijack333@gmail.com>
2019-12-06 00:34:37 +0800

    show list of files
    add FileList component and TableRow component

---
+++ frontend/src/components/dashboard/TableRow.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import fetchProgress from 'fetch-progress'
+import path from 'path';
+import upath from 'upath';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload, faCheck, faTimes, faUpload } from '@fortawesome/free-solid-svg-icons';
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+
+class TableRow extends Component {
+    constructor(props) {
+        super(props);
+        this.check = this.check.bind(this);
+        this.downloadFile = this.downloadFile.bind(this);
+
+        this.state = {
+            downloadProgress: 0
+        }
+    }
+
+    check() {
+        axios.post('/api/files/check/' + this.props.file._id)
+            .then(console.log('Checked or Unchecked'))
+            .catch(err => console.log(err));
+        window.location = '/file/list';
+    }
+
+    downloadFile() {
+        const filename = path.basename(upath.normalize(this.props.file.file_url));
+        const self = this;
+
+        fetch('/api/files/download/' + this.props.file._id)
+            .then(
+                fetchProgress({
+                    onProgress(progress) {
+                        self.setState({
+                            downloadProgress: progress.transferred / progress.total * 100
+                        })
+                    },
+                    onError(err) {
+                        console.log(err);
+                    },
+                    onComplete() {
+                        self.setState({
+                            downloadProgress: 0
+                        })
+                    }
+                })
+            )
+            .then(response => {
+                response.blob().then(blob => {
+                    let url = window.URL.createObjectURL(blob);
+                    let a = document.createElement('a');
+                    a.href = url;
+                    a.download = filename;
+                    a.click();
+                });
+            });
+    }
+
+    getDateStr(dateStr) {
+        if (dateStr === '' || !dateStr)
+            return '';
+        const date = new Date(dateStr);
+        return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+    }
+
+    render() {
+        const user = this.props.user;
+        const index = this.props.index;
+        const currentPage = this.props.currentPage;
+        const itemsPerPage = this.props.itemsPerPage;
+        return (
+            <tr key={index}>
+                <td style={{ width: "20px" }}>{(currentPage - 1) * itemsPerPage + index + 1}</td>
+                <td style={{ width: "140px" }}>{this.getDateStr(this.props.file.register_date)}</td>
+                <td style={{ width: "50px" }}>{this.props.file.owner}</td>
+                <td style={{ width: "90px" }}>{this.props.file.type}</td>
+                <td>{path.basename(upath.normalize(this.props.file.saved_url))}</td>
+                <td>{this.props.file.description}</td>
+                <td style={{ width: user.role === 'checker' || user.role === 'admin' ? "150px" : "52px" }}>
+                    <OverlayTrigger overlay={<Tooltip>Download</Tooltip>}>
+                        <Button onClick={this.downloadFile}>{this.state.downloadProgress === 0 ? <FontAwesomeIcon icon={faDownload} /> : `${Math.round(this.state.downloadProgress)}%`}</Button>
+                    </OverlayTrigger>
+                    {(user.role === "checker" || user.role === "admin") &&
+                        (<>
+                            <OverlayTrigger overlay={<Tooltip>Update</Tooltip>}>
+                                <Button onClick={this.upload} style={{ margin: "0 5px" }}><FontAwesomeIcon icon={faUpload} /></Button>
+                            </OverlayTrigger>
+                            <OverlayTrigger overlay={<Tooltip>{this.props.file.check_date ? "Set to unchecked" : "Set to checked" }</Tooltip>}>
+                                <Button onClick={this.check}>{this.props.file.check_date ? <FontAwesomeIcon icon={faTimes} /> : <FontAwesomeIcon icon={faCheck} />}</Button>
+                            </OverlayTrigger>
+                        </>)}
+                </td>
+            </tr>
+        );
+    }
+}
+export default TableRow;
